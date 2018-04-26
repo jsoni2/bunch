c74e8fda0704788ed4d7ce1b64ddb21e3b25f901
Janak Soni <jakzi7005@gmail.com>
2018-04-26 15:12:55 -0700

    IMPORT CUSTOMER AND RT COMOPNENTS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { FormsModule } from '@angular/forms';
 import { AuthGuard } from './auth.guard';
-import { ChartComponent } from './dashboard/chart/chart.component';
 import { TravelComponent } from './travel/travel.component';
 import { FlightComponent } from './travel/flight/flight.component';
 import { HotelComponent } from './travel/hotel/hotel.component';
+import { CustomerComponent } from './customer/customer.component';
+import { RottenTomatoesComponent } from './rotten-tomatoes/rotten-tomatoes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent,
-    ChartComponent,
     TravelComponent,
     FlightComponent,
-    HotelComponent
+    HotelComponent,
+    CustomerComponent,
+    RottenTomatoesComponent
   ],
   imports: [
     BrowserModule,
