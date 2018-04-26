8ab362478d04d7fddc80b50fa973b63b3f7c7084
Janak Soni <jakzi7005@gmail.com>
2018-04-26 15:09:31 -0700

    GENERATE COMPONENT : ROTTEN-TOMATOES

---
+++ src/app/rotten-tomatoes/rotten-tomatoes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rotten-tomatoes',
+  templateUrl: './rotten-tomatoes.component.html',
+  styleUrls: ['./rotten-tomatoes.component.css']
+})
+export class RottenTomatoesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
