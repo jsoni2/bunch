7870463150eedf3c102ab2b078037281afa34787
Janak Soni <jakzi7005@gmail.com>
2018-04-27 21:19:08 -0700

    CUSTOMER COMPONENT : ADD, UPDATE & DELETE

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Customers } from './customers'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  public customers = [];
+  public customer = new Customers();
+  public isEdit = false;
+  public currentIndex;
+  constructor(private route: Router) {
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+  ngOnInit() {
+
+  }
+
+  AddCustomer(){
+    this.customers.push(this.customer);
+    this.customer = {
+      firstName: '',
+      lastName: ''
+    }
+  }
+  deleteCustomer(element){
+    const index = this.customers.indexOf(element);
+    if(index !== -1){
+      this.customers.splice(index, 1);
+    }
+  }
+
+  editCustomer(element){
+    this.isEdit = true;
+    const index = this.customers.indexOf(element);
+    if(index !== -1){
+      this.customer.firstName = this.customers[index].firstName;
+      this.customer.lastName = this.customers[index].lastName;
+      this.currentIndex = index;
+    }
+  }
+  updateCustomer(){
+    this.customers.splice(this.currentIndex, 1, this.customer);
+    this.customer = {
+      firstName: '',
+      lastName: ''
+    }
+    this.currentIndex = undefined;
+    this.isEdit = false;
+  }
 }
---
+++ src/app/customer/customers.ts
@@ -0,0 +1,5 @@
+export class Customers {
+  firstName: string;
+  lastName: string;
+  
+}
